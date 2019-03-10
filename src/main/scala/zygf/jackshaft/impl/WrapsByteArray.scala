043b5cd12bfd1b78e394cffdc701ba8671d1cb5d
zygfryd <kat.zygfryd@gmail.com>
2019-03-10 19:50:02 +0100

    Removed unused code.

--- src/main/scala/zygf/jackshaft/impl/WrapsByteArray.scala
+++
@@ -1,7 +0,0 @@
-package zygf.jackshaft.impl
-
-abstract class WrapsByteArray[T >: Null]
-{
-  def copy(array: Array[Byte], offset: Int, length: Int): T
-  def wrap(array: Array[Byte]) = copy(array, 0, array.length)
-}
